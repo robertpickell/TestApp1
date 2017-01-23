0b7176643b4fa7a7483187c23c8a68c9ab2566c0
Wayne Pickell <wayne.pickell@gmail.com>
2017-01-23 18:41:24 -0500

    First commit

---
+++ file1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("The following arguments were passed to main():\n");
+    printf("argnum \t value \n");
+    for (int i = 0; i<argc; i++) printf("%d \t %s \n", i, argv[i]);
+    printf("\n");
+
+    return 0;
+} 
